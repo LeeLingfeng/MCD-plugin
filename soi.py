02aa5544648f410997d1e7f9027114c63c115d7b
LeeLingfeng <47819296+LeeLingfeng@users.noreply.github.com>
2020-02-07 17:26:12 +0800

    Update soi.py
    1.增加倒计时提醒
    2.指令必须白名单中的玩家才可以使用，白名单与pget插件相同

--- soi.py
+++ soi.py
@@ -2,6 +2,7 @@
 
 from time import sleep
 
+times = 5
 
 def onServerInfo(server, info):
   if info.isPlayer == 1:
@@ -11,6 +12,12 @@ def onServerInfo(server, info):
         for line in handle.readlines():
           whitelist.append(line.replace('\n','').replace('\r',''))
       if info.player in whitelist:
+        a = times
+        while a > 0:
+          server.say('服务器' + str(a) + '秒后重启')
+          sleep(1)
+          a -= 1
+        server.say('服务器重启')
         server.stop()
         sleep(10)
         server.start()
@@ -22,6 +29,12 @@ def onServerInfo(server, info):
         for line in handle.readlines():
           whitelist.append(line.replace('\n','').replace('\r',''))
       if info.player in whitelist:
+        a = times
+        while a > 0:
+          server.say('服务器' + str(a) + '秒后关闭')
+          sleep(1)
+          a -= 1
+        server.say('服务器关闭')
         server.stop()
       else:
         server.say('Permission denied')
