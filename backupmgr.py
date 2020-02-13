6ebc5da34b569c1a6490565cc951d5d974e58466
LeeLingfeng <47819296+LeeLingfeng@users.noreply.github.com>
2020-02-13 22:27:28 +0800

    完善功能和增加指令
    1.可以查询上次备份，以及进行还原
    2.!!bp name 名字
    备份并备注“名字”
       !!bp last
    查看上次备份
       !! bp restore
    还原

--- backupmgr.py
+++ backupmgr.py
@@ -2,6 +2,9 @@
 
 from savelib import mcsave
 import time
+import sys
+reload(sys) 
+sys.setdefaultencoding('utf-8')
 
 helpmsg = '''
 wtf is this shit
@@ -11,6 +14,7 @@ autoBackupEnabled = False
 autoBackupInterval = 24 #in hours
 skipIfSave = True
 savepath = './savelib'
+worldpath = './server/world'
 makebackup = mcsave(savepath)
 times = 5
 
@@ -25,18 +29,43 @@ def onServerInfo(server, info):
   if (info.isPlayer == 1) and (info.content.startswith('!!bp')):
     args = info.content.split(' ')
     if (len(args) == 1):
+      server.tell(info.player,'输入!!bp help查看帮助')
+    elif (len(args) == 2) and (args[1] == 'help'):
+      for line in helpmsg.splitlines():
+        server.tell(info.player, line)
+    elif (len(args) == 3) and (args[1] == 'name'):
+      meta = {}
+      meta['name'] = args[2]
       a = times
       while a > 0:
         server.say('服务器' + str(a) + '秒后开始备份')
         time.sleep(1)
         a -= 1
       server.say('开始备份')
-      meta = makebackup.backup()
-      if meta == {}:
+      meta = makebackup.backup(meta)
+      if meta['time'] == " ":
         server.say('备份失败')
       else :
         server.say('备份成功')
         makebackup.writeMeta(meta)
-    elif (len(args) == 2) and (args[1]=='help'):
-      for line in helpmsg.splitlines():
-        server.tell(info.player, line)
+    elif (len(args) == 2) and (args[1] == 'last'):
+      meta = makebackup.readMeta()
+      lastmsg = '最后一次备份时间是：' + meta['time'] + '，备注为：' + meta['name'] + ' '
+      server.tell(info.player,lastmsg)
+    elif (len(args) == 2) and (args[1] == 'restore'):
+      meta =  makebackup.readMeta()
+      if len(meta) == 2:
+        server.say('服务器即将还原备注为' + meta['name'] + '的备份')
+        a = times
+        while a > 0:
+          server.say('服务器' + str(a) + '秒后开始还原')
+          time.sleep(1)
+          a -= 1
+        server.say('开始还原')
+        server.stop()
+        time.sleep (5)
+        result = makebackup.restore(meta)
+        if result == 0:
+          server.start()
+      else:
+        server.say('备份记录有问题，请检查！')
--- savelib.py
+++ savelib.py
@@ -8,20 +8,26 @@ worldpath = './server/world'
 savepath = './savelib'
 
 class mcsave(object):
-  def __init__(self, savepath, meta = {}):
+  def __init__(self, savepath):
     self.path = savepath
-    with open(self.path + '/meta.json', 'w') as handler:
-      handler.write(json.dumps(meta))
   
   def backup(self, meta = {}, writeTime = True):
     if writeTime:
       clock = time.strftime('%Y%m%d',time.localtime()) + '-' + time.strftime('%H%M%S',time.localtime())
-    targetpath = savepath + '/' + clock
+    targetpath = self.path + '/' + clock
     os.system('mkdir ' + targetpath)
     result = os.system('cp -r ' + worldpath + ' ' + targetpath)
     if result == 0:
       meta['time'] = clock
+    else:
+      meta['time'] = " "
     return meta
+
+  def restore(self, meta):
+    targetpath = self.path + '/' + meta['time'] + '/world'
+    os.system('rm -rf ' + worldpath)
+    result = os.system('cp -r ' + targetpath + ' ' + './server/')
+    return result
 
   def readMeta(self):
     with open(self.path + '/meta.json', 'r') as handler:
